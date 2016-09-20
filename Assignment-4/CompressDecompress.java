27b946a8d82b3ac044b89412c2b5f5d2571a2043
Nick Miller <nam99@pitt.edu>
2016-09-19 20:01:17 -0400

    Add files via upload

---
+++ Assignment-4/CompressDecompress.java
@@ -0,0 +1,48 @@
+/**
+ * It is okay to use ArrayList class but you are not allowed to use any other
+ * predefined class supplied by Java.
+ */
+import java.util.ArrayList;
+
+public class CompressDecompress
+{
+	/**
+	 * Get a string representing a Huffman tree where its root node is root
+	 * @param root the root node of a Huffman tree
+	 * @return a string representing a Huffman tree
+	 */
+	public static String getTreeString(final BinaryNodeInterface<Character> root)
+	{
+		// TO DO
+
+		return "";	// Do not forget to change this line!!!
+	}
+
+	/**
+	 * Compress the message using Huffman tree represented by treeString
+	 * @param root the root node of a Huffman tree
+	 * @param message the message to be compressed
+	 * @return a string representing compressed message.
+	 */
+	public static String compress(final BinaryNodeInterface<Character> root, final String message)
+	{
+		// TO DO
+
+		return "";	// Do not forget to change this line!!!
+	}
+	
+	/**
+	 * Decompress the message using Huffman tree represented by treeString
+	 * @param treeString the string represents the Huffman tree of the
+	 * compressed message
+	 * @param message the compressed message to be decompressed
+	 * @return a string representing decompressed message
+	 */
+	public static String decompress(final String treeString, final String message)
+	{
+		// TO DO
+
+		return "";	// Do not forget to change this line!!!
+	}
+	
+}
